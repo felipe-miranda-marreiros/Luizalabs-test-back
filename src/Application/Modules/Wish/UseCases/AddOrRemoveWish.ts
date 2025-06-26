3d152b1ce547c58231dc407144f048273c6e1c0c
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2025-06-26 13:14:48 -0300

    refactor: update AddOrRemoveWishUseCase with wishListService and productService

--- src/Application/Modules/Wish/UseCases/AddOrRemoveWish.ts
+++ src/Application/Modules/Wish/UseCases/AddOrRemoveWish.ts
@@ -5,6 +5,9 @@ import {
   AddOrRemoveWishParams,
   WishList
 } from '@/Domain/Wish/UseCases/AddOrRemoveWish'
+import { wishListService } from '../Services/WishListLimitService'
+import { productService } from '../../Products/Services/ProductService'
+import { NotFoundError } from '@/Application/Contracts/Errors/NotFoundError'
 
 export class AddOrRemoveWishUseCase implements AddOrRemoveWish {
   constructor(
@@ -15,31 +18,61 @@ export class AddOrRemoveWishUseCase implements AddOrRemoveWish {
   async addOrRemove(params: AddOrRemoveWishParams): Promise<WishList> {
     const user = this.userContext.getLoggedInUser()
     const wishList = await this.wishRepository.getByUserId(user.id)
+    if (wishList) {
+      return this.updateExistingWishList(wishList, user.id, params.product_id)
+    }
+    return this.createWishList(user.id, params.product_id)
+  }
+
+  private async ensureProductExists(productId: number) {
+    const product = await productService.getById(productId)
+    if (!product) {
+      throw new NotFoundError('Produto não foi encontrado')
+    }
+    return product
+  }
+
+  private async updateExistingWishList(
+    wishList: { product_ids: number[] },
+    userId: number,
+    productId: number
+  ): Promise<WishList> {
+    const isInWishList = wishList.product_ids.includes(productId)
 
     let updatedProductIds: number[]
 
-    if (!wishList) {
-      updatedProductIds = [params.product_id]
-      await this.wishRepository.save({
-        user_id: user.id,
-        product_ids: updatedProductIds
-      })
+    if (isInWishList) {
+      updatedProductIds = wishList.product_ids.filter((id) => id !== productId)
     } else {
-      const exists = wishList.product_ids.includes(params.product_id)
-
-      updatedProductIds = exists
-        ? wishList.product_ids.filter((id) => id !== params.product_id)
-        : [...wishList.product_ids, params.product_id]
-
-      await this.wishRepository.update({
-        user_id: user.id,
-        product_ids: updatedProductIds
-      })
+      const product = await this.ensureProductExists(productId)
+      updatedProductIds = [...wishList.product_ids, product.id]
+      updatedProductIds = wishListService.checkForListLimit(updatedProductIds)
     }
 
+    await this.wishRepository.update({
+      user_id: userId,
+      product_ids: updatedProductIds
+    })
+
+    return this.buildWishList(updatedProductIds)
+  }
+
+  private buildWishList(productIds: number[]): WishList {
     return {
-      items: updatedProductIds,
-      count: updatedProductIds.length
+      items: productIds,
+      count: productIds.length
     }
+  }
+
+  private async createWishList(
+    userId: number,
+    productId: number
+  ): Promise<WishList> {
+    await this.wishRepository.save({
+      user_id: userId,
+      product_ids: [productId]
+    })
+
+    return this.buildWishList([productId])
   }
 }
